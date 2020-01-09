7174e9cfe8ec16b7fdb44da76654cf15860c56bc
altantic <15200582@life.hlkbu.edu.hk>
2020-01-09 22:50:05 +0800

    Implementing a "Attack" Method

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack() {
+            var max = 10;
+            var min = 3;
+            var demage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= demage;
+            if(this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRunning = false;
+                return; // end the function
+            }
+
+            max = 12;
+            min = 5;
+            demage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= demage;
+            if(this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRunning = false;
+            }
         }
     }
 });
