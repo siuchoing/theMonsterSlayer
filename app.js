7158f07ecf2a9c48e3ca52d37d2f053f77ff883d
altantic <15200582@life.hlkbu.edu.hk>
2020-01-09 23:14:14 +0800

    DRY principle for refactoring code for attack method

--- app.js
+++ app.js
@@ -13,24 +13,34 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack() {
-            var max = 10;
-            var min = 3;
-            var demage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.monsterHealth -= demage;
-            if(this.monsterHealth <= 0) {
-                alert('You won!');
-                this.gameIsRunning = false;
-                return; // end the function
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if (this.checkWin()) {
+                return; // end of function
             }
 
-            max = 12;
-            min = 5;
-            demage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.playerHealth -= demage;
-            if(this.playerHealth <= 0) {
-                alert('You lost!');
-                this.gameIsRunning = false;
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        calculateDamage(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin() {
+            if(this.monsterHealth <= 0) {
+                if(confirm('You won. New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if(confirm('You lost. New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
             }
+            return false;
         }
     }
 });
